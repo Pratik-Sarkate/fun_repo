b7d963f5373f2a374a65b42bbce115c264db4428
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2024-01-07 00:13:21 +0530

    Added the server code

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000
+
 function add(a,b){
 	return a + b;
 }
 
 console.log("Sum is = ", add(2,3));
+
+app.get("/", (req,res)=>{
+	let val = add(2,3);
+	res.send(val);
+});
+
+app.listen(PORT);
