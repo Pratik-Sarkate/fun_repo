63d7b0f88f916735dd5845500dfcf4649081a09a
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2024-01-07 00:18:44 +0530

    Made changes to index.js in feature_branch

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 
-function add(a,b){
-	return a + b;
-}
+function add(a,b) return a + b;
 
 console.log("Sum is = ", add(2,3));
 
